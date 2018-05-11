43d4d16d8ad9cb663608a01191f34ea6053ca6f6
Brian H <bheptin@gmail.com>
2018-05-11 13:42:44 -0500

    add axios BACK IN

--- src/components/AlbumList.js
+++ src/components/AlbumList.js
@@ -1,21 +1,29 @@
 import React, { Component } from 'react';
 import { View, Text } from 'react-native';
-
+import axios from 'axios';
 
 class AlbumList extends Component {
+    state: {
+      albums: []
+    }
+
 
 componentWillMount() {
-  fetch('https://rallycoding.herokuapp.com/api/music_albums')
-    .then((response) => response.json())
-    .then((responseData) => {
-      console.log(responseData);
-    });
+  axios.get('https://rallycoding.herokuapp.com/api/music_albums')
+  .then(response => this.setState({ albums: response.data }));
+}
+
+renderAlbums() {
+   return this.state.albums.map(album =>
+     <Text key={album.title}>{album.title}</Text>
+   );
 }
 
   render() {
+    console.log(this.state);
     return (
         <View>
-          <Text>Album List!!</Text>
+            {this.renderAlbums()}
         </View>
     );
   }
