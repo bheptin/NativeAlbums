8b99b9a4d3f4816b522ea42b0dee3bece0520c20
Brian H <bheptin@gmail.com>
2018-05-11 11:59:46 -0500

    remove Axios and use fetch method

--- src/components/AlbumList.js
+++ src/components/AlbumList.js
@@ -1,12 +1,24 @@
-import React from 'react';
+import React, { Component } from 'react';
 import { View, Text } from 'react-native';
 
-const AlbumList = () => {
-  return (
-      <View>
-        <Text>Album List!!</Text>
-      </View>
-  );
-};
+
+class AlbumList extends Component {
+
+componentWillMount() {
+  fetch('https://rallycoding.herokuapp.com/api/music_albums')
+    .then((response) => response.json())
+    .then((responseData) => {
+      console.log(responseData);
+    });
+}
+
+  render() {
+    return (
+        <View>
+          <Text>Album List!!</Text>
+        </View>
+    );
+  }
+}
 
 export default AlbumList;
