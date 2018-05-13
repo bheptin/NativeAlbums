2392dabfe36c70687e3dac64aed9de115e806d58
Brian H <bheptin@gmail.com>
2018-05-13 14:26:05 -0500

    add style, images, button

--- src/components/AlbumList.js
+++ src/components/AlbumList.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import { View } from 'react-native';
+import { ScrollView } from 'react-native';
 import axios from 'axios';
 import AlbumDetail from './AlbumDetail.js';
 
@@ -26,9 +26,9 @@ renderAlbums() {
   render() {
     console.log(this.state);
     return (
-        <View>
+        <ScrollView>
             {this.renderAlbums()}
-        </View>
+        </ScrollView>
     );
   }
 }
